4f91378667c0826fd8a187ca7bea50024e25241d
stanislas1200 <34397853+stanislas1200@users.noreply.github.com>
2023-12-02 16:59:15 +0100

    idk what to do

---
+++ ex00/src/main.cpp
@@ -0,0 +1,11 @@
+#include "../include/ScalarConverter.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 2) {
+		std::cout << "Usage: " << av[0] << " <string>" << std::endl;
+		return 1;
+	}
+	ScalarConverter::convert(av[1]);
+	return 0;
+}
